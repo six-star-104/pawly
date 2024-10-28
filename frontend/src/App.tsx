dcbc0d6787c18ed11420752d4822c453fd28735a
NANEUNSIK <skadmstlrek@naver.com>
2024-10-28 17:38:37 +0900

    fe/feat: 로그인 페이지 UI, 메인 페이지 구성중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import background from "./assets/background.png";
+import background from "./assets/images/background.png";
+import './App.css';
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import { Login } from "./pages/Login";
+import { Main } from "./pages/Main"
 
 function App() {
   return (
     <>
-      <img src={background} alt="" />
+      {/* <img src={background} alt="" /> */}
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
     </>
   );
 }
