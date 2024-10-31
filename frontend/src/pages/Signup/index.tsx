9a7df5d449545b7db38dbfd689bcb436777f54d1
허세령 <seryoii@naver.com>
2024-10-31 10:07:02 +0900

    fe/feat: 회원가입 페이지 추가

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,11 +1,15 @@
 import styles from "./style";
-import PixelContainer from "@/components/PixelContainer";
-
+import Pawly from "@/assets/images/Pawly.png";
 export const Signup = () => {
   return (
     <div>
       <div css={styles.container}>
-        <PixelContainer></PixelContainer>
+        <img src={Pawly} css={styles.logoImg} alt="Pawly Logo" />
+
+        <div css={styles.title}>닉네임을 입력해 주세요</div>
+        <input type="text" css={styles.nicknameInput} />
+        <button>중복 확인</button>
+        <button>다음</button>
       </div>
     </div>
   );
--- frontend/src/pages/Signup/style.ts
+++ frontend/src/pages/Signup/style.ts
@@ -1,15 +1,32 @@
 import { css } from "@emotion/react";
 
 export const container = css`
-  width: 100%;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  width: 100%;
+  height: 100vh;
   background-color: #f5eefc;
 `;
 
+export const logoImg = css`
+  margin-bottom: 2rem;
+`;
+
+export const title = css`
+  font-size: 1.25rem;
+`;
+
+export const nicknameInput = css`
+  border: none;
+  box-shadow: 4px 0 0 0 black, -4px 0 0 0 black, 0 4px 0 0 black,
+    0 -4px 0 0 black;
+  outline: none;
+  padding: 0.25rem;
+`;
 export default {
   container,
+  logoImg,
+  title,
+  nicknameInput,
 };
