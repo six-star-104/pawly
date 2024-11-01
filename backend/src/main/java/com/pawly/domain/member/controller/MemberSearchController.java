fe5f262b5a3d72f2a7b58b1709ae09c0c5446dac
신혜민 <alddlv81@gmail.com>
2024-11-01 13:17:28 +0900

    be/refactor: 검색 응답 형식 변경

--- backend/src/main/java/com/pawly/domain/member/controller/MemberSearchController.java
+++ backend/src/main/java/com/pawly/domain/member/controller/MemberSearchController.java
@@ -4,7 +4,6 @@ import com.pawly.domain.member.service.MemberSearchService;
 import com.pawly.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +15,7 @@ public class MemberSearchController {
     private final MemberSearchService memberSearchService;
 
     @GetMapping("/{nickname}")
-    public ResponseEntity<?> findMember(@PathVariable String nickname) {
-        ApiResponse<?> response = memberSearchService.memberSearch(nickname);
-        return ResponseEntity.ok(response);
+    public ApiResponse<?> findMember(@PathVariable String nickname) {
+        return memberSearchService.memberSearch(nickname);
     }
 }
