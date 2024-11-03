3e83ac05f89c1bd458c15c7eb4596f72e406770c
박민준 <ppmm98@naver.com>
2024-11-03 22:01:54 +0900

    be/feat: 회원 닉네임 관련 validation 검사

--- backend/src/main/java/com/pawly/domain/member/dto/request/UpdateNicknameRequestDTO.java
+++ backend/src/main/java/com/pawly/domain/member/dto/request/UpdateNicknameRequestDTO.java
@@ -1,10 +1,17 @@
 package com.pawly.domain.member.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UpdateNicknameRequestDTO {
 
+    @NotNull(message = "닉네임을 입력해주세요")
+    @Pattern(
+        regexp = "^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9\\s]{2,8}$",
+        message = "닉네임은 2~8자 사이의 한글, 영문(대소문자 구분 없음), 숫자만 허용되며, 중복된 문자는 사용할 수 없습니다."
+    )
     private String nickname;
 }
 
