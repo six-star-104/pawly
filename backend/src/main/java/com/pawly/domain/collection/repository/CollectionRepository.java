347cd212cf5835506f6cc3331ad3f9ea72fcc076
박민준 <ppmm98@naver.com>
2024-11-04 16:23:45 +0900

    be/feat: 도감 페이지네이션 구현

--- backend/src/main/java/com/pawly/domain/collection/repository/CollectionRepository.java
+++ backend/src/main/java/com/pawly/domain/collection/repository/CollectionRepository.java
@@ -2,6 +2,8 @@ package com.pawly.domain.collection.repository;
 
 import com.pawly.domain.collection.entity.Collection;
 import com.pawly.domain.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ import java.util.List;
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
 
     List<Collection> findByMemberId1(Member memberId);
+
+    Page<Collection> findByMemberId1(Member memberId, Pageable pageable);
 }
