03fe742e8546386ecafab5098a3f9431f10dd486
신혜민 <alddlv81@gmail.com>
2024-11-05 09:20:02 +0900

    be/feat: 친구 신청, 수락시 알림 추가

--- backend/src/main/java/com/pawly/domain/friend/service/FriendStatusService.java
+++ backend/src/main/java/com/pawly/domain/friend/service/FriendStatusService.java
@@ -5,9 +5,10 @@ import com.pawly.domain.friend.entity.FriendRequest;
 import com.pawly.domain.friend.repository.FriendRepository;
 import com.pawly.domain.friend.repository.FriendRequestRepository;
 import com.pawly.domain.member.entity.Member;
-import com.pawly.domain.member.repository.MemberRepository;
 import com.pawly.global.exception.ErrorCode;
 import com.pawly.global.response.ApiResponse;
+import com.pawly.global.dto.FcmMessageRequestDto;
+import com.pawly.global.service.FirebaseCloudMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ public class FriendStatusService {
 
     private final FriendRepository friendRepository;
     private final FriendRequestRepository friendRequestRepository;
+    private final FirebaseCloudMessageService firebaseCloudMessageService;
 
     @Transactional
     public ApiResponse<?> updateFriend(Long friendId, Boolean status) {
@@ -34,6 +36,10 @@ public class FriendStatusService {
             Friend friend = new Friend(sender, receiver);
             friendRepository.save(friend);
             friendRequestRepository.delete(friendRequest);
+
+            FcmMessageRequestDto request = new FcmMessageRequestDto(receiver.getMemberId(), "친구가 되어주셨네요!", "함께 소중한 추억을 만들어가세요");
+            firebaseCloudMessageService.sendMessage(request);
+
             return ApiResponse.createSuccessWithNoContent("친구 수락 성공");
         }
 
