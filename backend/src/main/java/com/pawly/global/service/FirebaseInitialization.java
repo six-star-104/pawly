e5a7ff42f5cbb99392e26184103f9a888917d08f
박민준 <ppmm98@naver.com>
2024-11-05 17:45:20 +0900

    be/feat: be build 오류 수정

--- backend/src/main/java/com/pawly/global/service/FirebaseInitialization.java
+++ backend/src/main/java/com/pawly/global/service/FirebaseInitialization.java
@@ -3,6 +3,7 @@ package com.pawly.global.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -12,10 +13,13 @@ import java.io.IOException;
 @Service
 public class FirebaseInitialization {
 
+    @Value("${firebase.config.path}")
+    private String firebasePath;
+
     @PostConstruct
     public void initialize() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 존재하지 않을 때만 초기화
-            FileInputStream serviceAccount = new FileInputStream("./src/main/resources/firebase-service-account.json");
+            FileInputStream serviceAccount = new FileInputStream(firebasePath);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
