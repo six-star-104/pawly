fbd480b8eb2be8f1c14978264826e4118fd7c344
신혜민 <alddlv81@gmail.com>
2024-11-05 09:13:36 +0900

    be/feat: 알림을 위한 fcm 로직 추가

---
+++ backend/src/main/java/com/pawly/global/service/FirebaseCloudMessageService.java
@@ -0,0 +1,37 @@
+package com.pawly.global.service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.pawly.domain.member.repository.MemberRepository;
+import com.pawly.global.dto.FcmMessageRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FirebaseCloudMessageService {
+
+    private final MemberRepository memberRepository;
+
+    public String sendMessage(FcmMessageRequestDto requestDto) {
+        // 사용자의 Firebase 토큰 값을 조회
+        String userFirebaseToken = memberRepository.findFcmTokenByMemberId(requestDto.getMemberId());
+
+        // 메시지 구성
+        Message message = Message.builder()
+                .putData("title", requestDto.getTitle())
+                .putData("content", requestDto.getContent())
+                .setToken(userFirebaseToken) // 조회한 토큰 값을 사용
+                .build();
+
+        try {
+            // 메시지 전송
+            String response = FirebaseMessaging.getInstance().send(message);
+            return "Message sent successfully: " + response;
+        } catch (FirebaseMessagingException e) {
+            e.printStackTrace();
+            return "Failed to send message";
+        }
+    }
+}
