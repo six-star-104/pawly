ea9f75fed9d93df1eb7ca85e08268f042b736d89
박민준 <ppmm98@naver.com>
2024-11-06 09:52:39 +0900

    be/fix: firebase 관련 경로 오류 수정

--- backend/src/main/java/com/pawly/global/service/FirebaseInitialization.java
+++ backend/src/main/java/com/pawly/global/service/FirebaseInitialization.java
@@ -3,6 +3,7 @@ package com.pawly.global.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -16,21 +17,20 @@ import java.io.IOException;
 @Service
 public class FirebaseInitialization {
 
-    @Value("${firebase.config.path}")
-    private String firebasePath;
-
-    @Autowired
-    private ResourceLoader resourceLoader;
-
-
     @PostConstruct
     public void initialize() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 존재하지 않을 때만 초기화
-            Resource resource = resourceLoader.getResource(firebasePath);
-            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+            String firebaseConfigPath = "firebase-service-account.json";
+
+            // 리소스를 InputStream으로 읽기
+            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream(firebaseConfigPath);
+            if (serviceAccount == null) {
+                throw new IOException("Firebase configuration file not found: " + firebaseConfigPath);
+            }
+
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
             FirebaseApp.initializeApp(options);
         }
