9a0c648854312b29c9d9c7bad543d9396a875796
최형우 <chlguddn1212@naver.com>
2024-11-07 10:55:48 +0900

    fe/feat: 테마 추가

--- frontend/src/types/rollingPaperTypes.ts
+++ frontend/src/types/rollingPaperTypes.ts
@@ -1,3 +1,13 @@
+export interface ITheme {
+  backgroundColor: number;
+  fontColor: number;
+  borderColor: number;
+  image: string;
+  themeName: string;
+  base: boolean;
+  status: boolean;
+}
+
 export interface IPostIt {
   postItId?: number;
   memberId?: number;
@@ -5,11 +15,20 @@ export interface IPostIt {
 
   content: string;
   status?: string;
+
+  // 이 4개가 DTO로 받을수도 있고,
   backgroundColor: number;
   fontColor: number;
   borderColor: number;
   image: string;
+  // 2개 추가로 생길 예정
+  // font: number;
+  // themeName: string;
+  // base:boolean으로 올수도??
+  // status:boolean  미션 달성여부
+
   speechBubbleSize: number;
+
   createdAt?: string;
   updatedAt?: string;
 }
