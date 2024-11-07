1df93b53924af6b7282df23a8798ab5c5aed08b4
박민준 <ppmm98@naver.com>
2024-11-07 13:06:36 +0900

    be/fix: 편지함 내용 미리보기로 인한 DTO 수정

--- backend/src/main/java/com/pawly/domain/letter/dto/response/SendLetterResponseDTO.java
+++ backend/src/main/java/com/pawly/domain/letter/dto/response/SendLetterResponseDTO.java
@@ -12,6 +12,7 @@ public class SendLetterResponseDTO {
     private Long sendLetterId;
     private Long recipientId;
     private String recipientName;
+    private String content;
     private Long letterId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
@@ -21,6 +22,7 @@ public class SendLetterResponseDTO {
             .sendLetterId(sendLetter.getSendLetterId())
             .recipientId(sendLetter.getLetter().getRecipient().getMemberId())
             .recipientName(sendLetter.getLetter().getRecipient().getNickname())
+            .content(sendLetter.getLetter().getContent())
             .letterId(sendLetter.getLetter().getLetterId())
             .createdAt(sendLetter.getLetter().getCreatedAt())
             .updatedAt(sendLetter.getLetter().getUpdatedAt())
