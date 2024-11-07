af7da3b485ec6f35f6797b44a134aa5fdb946d4b
Kim jongdeok <whd4638@naver.com>
2024-11-07 16:05:08 +0900

    be/feat: 포스트박스 인접거리 생성 금지

--- backend/src/main/java/com/pawly/domain/postbox/repository/PostboxRepository.java
+++ backend/src/main/java/com/pawly/domain/postbox/repository/PostboxRepository.java
@@ -1,5 +1,6 @@
 package com.pawly.domain.postbox.repository;
 
+import com.pawly.domain.member.entity.Member;
 import com.pawly.domain.postbox.entity.Postbox;
 import com.pawly.domain.rollingPaper.entity.RollingPaper;
 import io.lettuce.core.dynamic.annotation.Param;
@@ -7,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PostboxRepository extends JpaRepository<Postbox, Long> {
     @Query(value = "SELECT p FROM Postbox p WHERE " +
@@ -18,6 +18,17 @@ public interface PostboxRepository extends JpaRepository<Postbox, Long> {
     List<Postbox> findPostboxesWithinRadius(@Param("latitude") Double latitude,
                                             @Param("longitude") Double longitude,
                                             @Param("radius") Double radius);
+
+    @Query(value = "SELECT p FROM Postbox p WHERE " +
+            "(6371000 * acos(cos(radians(:latitude)) * cos(radians(p.latitude)) * " +
+            "cos(radians(p.longitude) - radians(:longitude)) + sin(radians(:latitude)) * " +
+            "sin(radians(p.latitude)))) <= :radius " +
+            "AND p.member =:member " +
+            "AND p.status = 'USE'")
+    List<Postbox> findPostboxesByMemberWithinRadius(@Param("member") Member member,
+                                                    @Param("latitude") Double latitude,
+                                                    @Param("longitude") Double longitude,
+                                                    @Param("radius") Double radius);
 
     Postbox findByRollingpaper(RollingPaper rollingPaper);
 }
