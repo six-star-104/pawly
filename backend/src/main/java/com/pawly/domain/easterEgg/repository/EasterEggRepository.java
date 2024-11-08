515057250a558893876b01e25d7fc3e71ab04449
신혜민 <alddlv81@gmail.com>
2024-11-08 14:58:31 +0900

    be/feat: 테마 조회

--- backend/src/main/java/com/pawly/domain/easterEgg/repository/EasterEggRepository.java
+++ backend/src/main/java/com/pawly/domain/easterEgg/repository/EasterEggRepository.java
@@ -2,6 +2,13 @@ package com.pawly.domain.easterEgg.repository;
 
 import com.pawly.domain.easterEgg.entity.EasterEgg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface EasterEggRepository extends JpaRepository<EasterEgg, Long> {
+
+    @Query("SELECT e.easterEggId FROM EasterEgg e " +
+            "WHERE e.theme.themeId =:themeId")
+    Optional<Long> findByEasterEggId(Long themeId);
 }
---
+++ backend/src/main/java/com/pawly/domain/theme/dto/ThemeResponseDto.java
@@ -0,0 +1,29 @@
+package com.pawly.domain.theme.dto;
+
+import com.pawly.domain.theme.entity.Theme;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class ThemeResponseDto {
+
+    private Long theme;
+    private String themeName;
+
+    @Setter
+    private String background;
+    private String fontColor;
+    private String borderColor;
+
+    @Setter
+    private boolean flag;
+
+    public ThemeResponseDto(Theme theme) {
+        this.theme = theme.getThemeId();
+        this.themeName = theme.getThemeName();
+        this.fontColor = theme.getFontColor();
+        this.borderColor = theme.getBorderColor();
+    }
+}
