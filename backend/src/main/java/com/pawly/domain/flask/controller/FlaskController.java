b558c8cc0274984470c6fab269941e087a21c5dd
박민준 <ppmm98@naver.com>
2024-11-08 17:24:39 +0900

    be/feat: 응답 수정

--- backend/src/main/java/com/pawly/domain/flask/controller/FlaskController.java
+++ backend/src/main/java/com/pawly/domain/flask/controller/FlaskController.java
@@ -6,9 +6,12 @@ import com.pawly.global.exception.ErrorCode;
 import com.pawly.global.response.ApiResponse;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,11 +23,11 @@ public class FlaskController {
     private final DynamicScheduler dynamicScheduler;
 
     @GetMapping
-    public CompletableFuture<ApiResponse<String>> processWord(@RequestParam String word) {
+    public CompletableFuture<ResponseEntity<String>> processWord(@RequestParam String word) {
         dynamicScheduler.adjustScheduler(); // 요청이 올 때마다 스케줄 조정
         return flaskService.sendRequestToFlask(word)
-            .thenApply(result -> ApiResponse.createSuccess(result, "요청 성공"))
-            .exceptionally(ex -> ApiResponse.createError(ErrorCode.TOO_MANY_REQUEST));
+            .thenApply(result -> new ResponseEntity<>(result, HttpStatus.OK))
+            .exceptionally(ex -> new ResponseEntity<>("error", HttpStatus.BAD_REQUEST));
     }
 }
 
