70eb172421f72179aee1fe4de670cec8f277b97f
최형우 <chlguddn1212@naver.com>
2024-11-12 14:10:45 +0900

    fe/fix: svgr관련 셋팅 수정

--- frontend/src/svg.d.ts
+++ frontend/src/svg.d.ts
@@ -1,6 +1,8 @@
 // svg 파일 사용위한 커스텀
 declare module "*.svg" {
   import React = require("react");
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default content;
+  
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string
+  export default src;
 }
