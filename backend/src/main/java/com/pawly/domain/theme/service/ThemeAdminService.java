e07ef26319e5c0b47e6cd3f6d4c29e5dc040b59e
Kim jongdeok <whd4638@naver.com>
2024-11-13 15:44:55 +0900

    be/feat: theme 조회(관리자용) 기능 추가

--- backend/src/main/java/com/pawly/domain/theme/service/ThemeAdminService.java
+++ backend/src/main/java/com/pawly/domain/theme/service/ThemeAdminService.java
@@ -6,6 +6,7 @@ import com.pawly.domain.member.entity.Member;
 import com.pawly.domain.member.entity.Role;
 import com.pawly.domain.member.repository.MemberRepository;
 import com.pawly.domain.member.service.MemberService;
+import com.pawly.domain.theme.dto.ThemeAdminReadDto;
 import com.pawly.domain.theme.dto.ThemeCreateDto;
 import com.pawly.domain.theme.dto.ThemeUpdateDto;
 import com.pawly.domain.theme.entity.Theme;
@@ -13,9 +14,11 @@ import com.pawly.domain.theme.repository.ThemeRepository;
 import com.pawly.global.exception.ErrorCode;
 import com.pawly.global.response.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -68,5 +71,22 @@ public class ThemeAdminService {
 
         theme.get().deleteTheme();
         return ApiResponse.createSuccessWithNoContent("테마 삭제 성공");
+    }
+
+    @Transactional
+    public ApiResponse<?> readTheme(String name) {
+        Optional<Member> member = memberRepository.findByEmail(name);
+        if (member.isEmpty()) return ApiResponse.createError(ErrorCode.THEME_NOT_FOUND);
+
+        Member m = member.get();
+
+        if (!m.getRole().equals(Role.ADMIN)) return ApiResponse.createError(ErrorCode.ACCESS_DENIED);
+
+        List<Theme> themeList = themeRepository.findAll();
+        List<ThemeAdminReadDto> themeAdminReadDtos = themeList.stream()
+                .map(ThemeAdminReadDto::of)
+                .toList();
+
+        return ApiResponse.createSuccess(themeAdminReadDtos,"테마 조회 성공");
     }
 }
