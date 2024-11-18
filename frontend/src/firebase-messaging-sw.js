eb3daf89495c7b1e72068256061f840ee7370cb6
최형우 <chlguddn1212@naver.com>
2024-11-18 10:40:03 +0900

    fe/fix: 알람 중복 제거

--- frontend/src/firebase-messaging-sw.js
+++ frontend/src/firebase-messaging-sw.js
@@ -63,18 +63,18 @@ async function requestPermission() {
   });
 }
 
-self.addEventListener("push", function (e) {
-  if (!e.data.json()) return;
+// self.addEventListener("push", function (e) {
+//   if (!e.data.json()) return;
 
-  const resultData = e.data.json().notification;
-  const notificationTitle = resultData.title;
-  const notificationOptions = {
-    body: resultData.body,
-    // icon: logo, // 웹 푸시 이미지는 icon
-    // tag: resultData.tag,
-  };
-  console.log("활성중 알람", notificationOptions.body)
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//   const resultData = e.data.json().notification;
+//   const notificationTitle = resultData.title;
+//   const notificationOptions = {
+//     body: resultData.body,
+//     // icon: logo, // 웹 푸시 이미지는 icon
+//     // tag: resultData.tag,
+//   };
+//   console.log("활성중 알람", notificationOptions.body)
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
 requestPermission();
